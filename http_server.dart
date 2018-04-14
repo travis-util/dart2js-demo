a6613bc1ca42ab7f56e3c0b396aaab79055eb3a9
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 16:39:08 -0400

    Update http_server.dart

--- http_server.dart
+++ http_server.dart
@@ -7,11 +7,11 @@ Future main(List<String> arguments) async {
     exit(1); // Exists all threads! TODO
   }
   if (1 == arguments.length) {
-    print('Starting server on http://localhost:');
-    print(int.parse(arguments[0]));
+    // print('Starting server on http://localhost:');
+    // print(int.parse(arguments[0]));
   }
   if (0 == arguments.length) {
-    print('Starting server on http://localhost:8888...');
+    // print('Starting server on http://localhost:8888...');
   }
   final requests = await HttpServer.bind('localhost', (0 == arguments.length) ? 8888 : int.parse(arguments[0]));
   await for (var request in requests) {
