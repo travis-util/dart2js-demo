15312ad187ffb1a8a48c5581ca34cd2cf00e59b1
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 12:32:57 -0400

    Create http_server.dart

---
+++ http_server.dart
@@ -0,0 +1,24 @@
+Future main() async {
+  final requests = await HttpServer.bind('localhost', 8888);
+  await for (var request in requests) {
+    processRequest(request);
+  }
+}
+
+void processRequest(HttpRequest request) {
+  print('Got request for ${request.uri.path}');
+  final response = request.response;
+  if (request.uri.path == '/dart') {
+    response
+      ..headers.contentType = new ContentType(
+        'text',
+        'plain',
+      )
+      ..write('Hello from the server');
+  } else {
+    response.statusCode = HttpStatus.NOT_FOUND;
+  }
+  response.close();
+}
+
+// From https://www.dartlang.org/dart-vm/io-library-tour
