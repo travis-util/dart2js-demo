467d043615fbd9a8e6444fa04949918ab111ffe2
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 17:32:40 -0400

    Update print_arguments.dart

--- print_arguments.dart
+++ print_arguments.dart
@@ -4,4 +4,5 @@ main(List<String> arguments) {
   print(arguments);
   // stdout.writeln('$arguments...(stdout)'); // node: Unsupported operation: StdIOUtils._getStdioOutputStream
   // https://www.dartlang.org/tutorials/dart-vm/cmdline#stdout
+  // However, you must use print() instead of stdout for programs that are converted to and run in JavaScript.
 }
