e6b13a0c5535eb26fd5dd19ce53ede1b04ee8c3f
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 17:31:48 -0400

    Update print_arguments.dart

--- print_arguments.dart
+++ print_arguments.dart
@@ -3,4 +3,5 @@
 main(List<String> arguments) {
   print(arguments);
   // stdout.writeln('$arguments...(stdout)'); // node: Unsupported operation: StdIOUtils._getStdioOutputStream
+  // https://www.dartlang.org/tutorials/dart-vm/cmdline#stdout
 }
