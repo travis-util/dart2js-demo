414a1ad97ef3945a5c7c6b220738b1c366e180ec
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 10:17:47 -0400

    Create fibonacci.dart

---
+++ fibonacci.dart
@@ -0,0 +1,7 @@
+int fib(int n) => (n > 2) ? (fib(n - 1) + fib(n - 2)) : 1;
+
+void main() {
+  print('fib(20) = ${fib(20)}');
+}
+
+// From https://en.wikipedia.org/wiki/Dart_(programming_language)#Example
