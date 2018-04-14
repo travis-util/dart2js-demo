0b4ff89ca74f196ea02ab243d523a073f65248bc
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 14:53:36 -0400

    Update http_server.dart

--- http_server.dart
+++ http_server.dart
@@ -1,19 +1,11 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:args/args.dart';
-
 Future main(List<String> arguments) async {
-  // final parser = new ArgParser();
-  // argResults = parser.parse(arguments);
-  // List<String> args = argResults.rest;
-  // print(arguments.length);
   if (1 != arguments.length) {
     print('Usage: <commande file name> <port number>');
     exit(1); // Exists all threads! TODO
   }
-  // print(arguments);
-  // print(int.parse(arguments[0]));
   final requests = await HttpServer.bind('localhost', int.parse(arguments[0]));
   await for (var request in requests) {
     processRequest(request);
