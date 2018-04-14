ad49524975e5c7616d6e7b753312ed3571738995
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-14 17:29:12 -0400

    Update print_arguments.dart

--- print_arguments.dart
+++ print_arguments.dart
@@ -1,6 +1,6 @@
-import 'dart:io'; // stdout
+// import 'dart:io'; // stdout
 
 main(List<String> arguments) {
   print(arguments);
-  stdout.writeln('$arguments...(stdout)');
+  // stdout.writeln('$arguments...(stdout)'); // node: Unsupported operation: StdIOUtils._getStdioOutputStream
 }
